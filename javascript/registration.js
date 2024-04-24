2fa34c174b7bb90a4f854cf358154af114c34814
Zain-Balkhi <75040432+Zain-Balkhi@users.noreply.github.com>
2024-04-23 22:44:48 -0400

    DATABASE WORKS NOW, WE DONE
    got database implemented across files. Everything is hidden so we don't get hacked and lose our API credits

--- javascript/registration.js
+++ javascript/registration.js
@@ -13,6 +13,19 @@ function registerUser() {
     .then(response => {
         if (response.ok) {
             // Registration successful
+
+            // PUT IN REAL MONGOCLIENT AND URL ON EACH PULL, PUT IN DUMMY ON EACH PUSH, DON'T WANT PI ON PUBLIC REPO    
+            var MongoClient = require('mongodb').MongoClient;
+            var url = "duh";
+            MongoClient.connect(url, function(err, db) {
+                if (err) throw err;
+                var dbo = db.db("mydb");
+                var myobj = { username: newUsername, password: newPassword };
+                dbo.collection("users").insertOne(myobj, function(err, res) {
+                  if (err) throw err;
+                  db.close();
+                });
+              });
             // Redirect the user to the login page or show a success message
             window.location.href = 'login.html';
         } else {
